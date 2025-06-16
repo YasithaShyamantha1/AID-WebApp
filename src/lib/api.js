e31acd7a70964629a9f410f7590e3aea9b365970
IT22608604 <yasithawolf75@gmail.com>
2025-06-16 19:41:31 +0530

    fix backend endpoint

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 // const BACKEND_URL = "https://aid-web-backend-new.onrender.com";
-const BACKEND_URL="http://localhost:8000/";             
+const BACKEND_URL="http://localhost:8000";             
 
 export const api = createApi({
   reducerPath: "api",
